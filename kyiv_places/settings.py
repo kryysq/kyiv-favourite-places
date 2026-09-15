cbe8ac69c9c84a81f70744bc31e694c45e7cce2b
kryysq <ivanenkokristina0@gmail.com>
2026-09-15 11:30:29 +0300

    Add home page

--- kyiv_places/settings.py
+++ kyiv_places/settings.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = 'kyiv_places.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'places' / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- kyiv_places/urls.py
+++ kyiv_places/urls.py
@@ -1,4 +1,8 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('places.urls')),
 ]
