f1c92796e7870d98e910425b98e6ebb5bd5780d6
kryysq <ivanenkokristina0@gmail.com>
2026-09-15 15:23:29 +0300

    Add place creation view

--- places/views.py
+++ places/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import PlaceForm
+from .models import Place
 
 def home(request):
-    return render(request, 'places/home.html')
+    places = Place.objects.all()
+
+    return render(
+        request,
+        'places/home.html',
+        {'places': places}
+    )
+
+
+def add_place(request):
+    if request.method == 'POST':
+        form = PlaceForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PlaceForm()
+
+    return render(
+        request,
+        'places/add_place.html',
+        {'form': form}
+    )
